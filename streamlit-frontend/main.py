126459bda77a915dc0f75320d96e7325be0dc2dc
jeIIycodes <anthonycastillolmk@gmail.com>
2024-09-28 13:08:41 -0400

    test

--- streamlit-frontend/main.py
+++ streamlit-frontend/main.py
@@ -13,12 +13,11 @@ def login(username, password):
     return False
 
 # Streamlit app
-st.title('Login Page')
-
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
+    st.title('FIU Women Financial Advisory')
     username = st.text_input('Username')
     password = st.text_input('Password', type='password')
     if st.button('Login'):
@@ -27,15 +26,19 @@ if not st.session_state['logged_in']:
             st.success('Login successful')
         else:
             st.error('Invalid username or password')
-else:
-    st.title('Dashboard')
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.header('Box 1')
-        st.write('Content for box 1')
-    with col2:
-        st.header('Box 2')
-        st.write('Content for box 2')
-    with col3:
-        st.header('Box 3')
-        st.write('Content for box 3')
+            
+if st.session_state['logged_in']:
+    
+    st.sidebar.title('Navigation')
+    
+    page = st.sidebar.radio("Go to", ["Profile", "Lets Talk Data", "Scholarships", "Resources"])
+    
+    if page == "Profile":
+        st.title("Profile")
+        st.header('Welcome to your profile, name!')
+        
+    elif page == "Lets Talk Data":
+        pass #do stuff
+    elif page == "Scholarships":
+        pass #do stuff
+    
