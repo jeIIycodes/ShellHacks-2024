1b363490a77276d3352d5cad17c2160a8d12c002
Anne Nguyen <anguy031@fiu.edu>
2024-09-28 18:18:37 +0000

    Beginning of file reader started + comments

---
+++ python-ai-backend/app/reader.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+#Plan of this file is to be able to read the two xlsx files and be able to work with the applicant data
+#Make sure that the data is adequately preprocessed first
+#For detail in data used, we will target the Detailed rows under O_GROUP if there is a duplicate
+
