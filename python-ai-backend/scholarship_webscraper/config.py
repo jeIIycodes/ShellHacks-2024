027acec5db66c36e753719891fd27678a689380b
michaelbennie <michaelbennie@ufl.edu>
2024-09-28 16:31:16 -0400

    make webscraper for scholarships

---
+++ python-ai-backend/scholarship_webscraper/config.py
@@ -0,0 +1,10 @@
+# config_template.py
+
+
+# Selenium WebDriver Configuration
+GECKODRIVER_PATH = "/usr/local/bin/geckodriver"
+FIREFOX_BINARY_PATH = "/usr/lib/firefox/firefox"
+
+
+# Additional Configuration (if any)
+# Add other configuration variables as needed
