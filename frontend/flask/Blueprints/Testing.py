7b9462df4e9a34df394393856ab60a49e573468a
jeIIycodes <anthonycastillolmk@gmail.com>
2024-09-28 20:59:33 -0400

    I described the student spending dataset

--- frontend/flask/Blueprints/Testing.py
+++ frontend/flask/Blueprints/Testing.py
@@ -3,20 +3,6 @@ from flask import Blueprint, jsonify, render_template
 
 test_bp=Blueprint("testingStuff", __name__, url_prefix="/testing")
 
-@test_bp.route('/averages')
-def show_averages():
-    """
-    Route to display the averages of each column.
-    """
-    # Path to the CSV file
-    csv_path = 'static/student_spending.csv'
-
-    # Calculate averages
-    averages = calculate_averages(csv_path)
-
-    return render_template('averages.html', averages=averages)
-
-
 @test_bp.route('/TestRoute')
 def helloWorld():
     return jsonify({"Hello": "World"})
